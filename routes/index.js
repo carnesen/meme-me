48d38b7bf3f3b5054d3fb401da0e97e84cb5b54e
Chris Arnesen <chris.arnesen@gmail.com>
2015-11-25 18:06:54 -0600

    gallery home page works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var path = require('path');
+
+var images = require('../lib/images');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Meme-Me', images: images });
 });
 
 module.exports = router;
